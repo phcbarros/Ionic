f306e05790f9007840f028404a223e45f8672fae
Paulo Henrique da Costa Barros <paulo_costinha@hotmail.com>
2015-11-20 14:31:44 -0200

    CSSComponents - Toggles

---
+++ CSSComponents/www/app/toggle/ToggleCtrl.js
@@ -0,0 +1,13 @@
+angular.module("starter")
+	.controller("ToggleCtrl", ToggleCtrl);
+
+function ToggleCtrl($scope){
+	$scope.toggles = [
+		{ content: "HTML", color: "toggle toggle-assertive", checked: true },
+		{ content: "CSS3", color: "toggle toggle-calm", checked: true },
+		{ content: "Flashplayer", color: "toggle toggle-balanced", checked: false },
+		{ content: "Java Applets", color: "toggle toggle-energized", checked: false },
+		{ content: "Javascript", color: "toggle toggle-dark", checked: true },
+		{ content: "Silverlight", color: "toggle toggle-positive", checked: false }
+	]
+};
